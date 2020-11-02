c4f85110c078675452cde9278cbf977804135202
arnabGudu <arnabgudu.gs@gmail.com>
2020-11-02 20:32:42 +0530

    final commit

--- sahayak_bot/ebot_controller/scripts/controller.py
+++ sahayak_bot/ebot_controller/scripts/controller.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 '''
-Eyantra
+Ebot controller node, contains all algorithms to follow a sinosodial trajectory
+with Odometry, avoid obstacles using LaserScan, and publish Proportional control
+command to skid steer drive to control the robot.
 '''
 import math
 import rospy
@@ -9,11 +11,11 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
-POSE = [0, 0, 0]
-REGIONS = dict()
-P = 0.8
-K = 0.5
-NUM_POINTS = 20
+POSE = list()               # List to contain Odometry data
+REGIONS = dict()            # Dict to contain LaserScan data
+P = 1                       # Proportionality constant
+K = 2                       # Obstacle avoidance constant
+NUM_POINTS = 20             # Number of keypoints between 0, 2 PI
 
 def waypoints(x_1):
     '''
@@ -21,12 +23,12 @@ def waypoints(x_1):
     params: t (float): x coordinte of ebot, POSE[0]
     return: [x, y] (list): next generated coordintes
     '''
-    if x_1 > 2 * math.pi:
-        x_2, y_2 = 12.5, 0
+    if x_1 > 2 * math.pi:                           # If bot crossed 2PI
+        x_2, y_2 = 12.5, 0                          # then goal is (12.0, 0)
     else:
-        mul = 2 * math.pi / NUM_POINTS
-        x_2 = (x_1 // mul + 1) * mul
-        y_2 = 2 * math.sin(x_2) * math.sin(x_2 / 2)
+        mul = 2 * math.pi / NUM_POINTS              # Factor 2PI/NUM_POINTS
+        x_2 = (x_1 // mul + 1) * mul                # generate a point ahead
+        y_2 = 2 * math.sin(x_2) * math.sin(x_2 / 2) # y_coord from equation
     return [x_2, y_2]
 
 def control_loop():
@@ -35,11 +37,9 @@ def control_loop():
     trajectory following algorithm and P controller
     '''
     rospy.init_node('ebot_controller')
-
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.Subscriber('/ebot/laser/scan', LaserScan, laser_callback)
     rospy.Subscriber('/odom', Odometry, odom_callback)
-
     rate = rospy.Rate(10)
 
     velocity_msg = Twist()
@@ -50,29 +50,34 @@ def control_loop():
     while not rospy.is_shutdown():
         x_1, y_1, ebot_theta = POSE
         x_2, y_2 = waypoints(x_1)
-        theta_goal = math.atan((y_2 - y_1)/(x_2 - x_1))
-        e_theta = ebot_theta - theta_goal
 
-        state = 'traj'
-        final_dist, laser_error = 0, 0
+        # Go to goal controller
+        theta_goal = math.atan2(y_2 - y_1, x_2 - x_1)
+        e_theta = theta_goal - ebot_theta
+        balance = P * e_theta
+
         if x_1 > 2 * math.pi:
-            state = 'wall'
-            laser_error = 1/REGIONS['fleft'] - 1/REGIONS['front'] - 1/REGIONS['fright']
-            e_theta = e_theta + K * laser_error
+            # For obstacle avoidance, a non-linear approach is used
+            r_err = 1/(REGIONS['fright'])
+            l_err = 1/(REGIONS['fleft'])
+            f_err = 1/(REGIONS['front'])
+            lb_err = 1/(REGIONS['bleft'])
+            rb_err = 1/(REGIONS['bright'])
+            laser_error = r_err - l_err + 2 * f_err - rb_err/1.5 + lb_err/1.5
 
+            # The above constants are experimentally determined
+            # A weighted sum of goal error and obstacle error
+            balance = P * e_theta + K * (laser_error)
+
+            # Check if goal is reached, based on euclidean distance
             final_dist = math.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
-            if final_dist < 0.5:
+            if final_dist < 0.1:
                 break
 
-        print("x_1: {0:f}, y_1: {1:f}, x_2: {2:f}, y_2: {3:f}, ebot: {4:f}, \
-               gl: {5:f}, er: {6:f}".format(x_1, y_1, x_2, y_2, ebot_theta,
-                                            theta_goal, e_theta))
-        print "{0:s}, l_e: {1:f}, e_t: {2:f}, \
-               f_d: {3:f}".format(state, laser_error, e_theta, final_dist)
-        velocity_msg.linear.x = 0.1
-        velocity_msg.angular.z = -P * e_theta
+        velocity_msg.linear.x = 0.2
+        velocity_msg.angular.z = balance
         pub.publish(velocity_msg)
-        # print("Controller message pushed at {}".format(rospy.get_time()))
+        print("Controller message pushed at {}".format(rospy.get_time()))
         rate.sleep()
 
     velocity_msg.linear.x = 0
@@ -81,21 +86,23 @@ def control_loop():
 
 def odom_callback(data):
     '''
-    Callback function for /odom topic
+    Callback function for /odom topic, assign Odometry data like x and y
+    coori and euler angle to POSE variable.
     params: msg (nav_msgs/Odometry): Odometry data from ebot
     return: None
     '''
     global POSE
-    q_x = data.POSE.POSE.orientation.x
-    q_y = data.POSE.POSE.orientation.y
-    q_z = data.POSE.POSE.orientation.z
-    q_w = data.POSE.POSE.orientation.w
-    POSE = [data.POSE.POSE.position.x, data.POSE.POSE.position.y,
+    q_x = data.pose.pose.orientation.x
+    q_y = data.pose.pose.orientation.y
+    q_z = data.pose.pose.orientation.z
+    q_w = data.pose.pose.orientation.w
+    POSE = [data.pose.pose.position.x, data.pose.pose.position.y,
             euler_from_quaternion([q_x, q_y, q_z, q_w])[2]]
 
 def laser_callback(msg):
     '''
-    Callback function for /ebot/laser/scan topic
+    Callback function for /ebot/laser/scan topic, the function calculates min
+    distance in a specific range and assign it to global variable REGIONS.
     params: msg (sensor_msgs/LaserScan): laser sensor data and ranges
     return: None
     '''
